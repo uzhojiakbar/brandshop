c27285cbd0799bd85bd1f4f3162249c8a2c46c35
uzhojiakbar3 <uzhojiakbar3@gmail.com>
2023-12-24 11:22:01 +0500

    project started / 12/24/2023

---
+++ src/Root/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { MainRoot } from './style'
+import './design.css'
+
+const Root = () => {
+    return (
+        <MainRoot className='main'>
+            Root
+        </MainRoot>
+    )
+}
+
+export default Root
---
+++ src/Root/style.js
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+const MainRoot = styled.div``;
+
+export { MainRoot };
